c591974a7d6b75791569ecdb827ab1f9b57202bf
Marco Lechner, GeoBoink <marco@marcolechner.de>
2022-04-07 22:05:54 +0200

    WIP

--- irmis_json_loader_dialog.py
+++ irmis_json_loader_dialog.py
@@ -23,6 +23,7 @@
 """
 
 import os
+from datetime import datetime
 
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
@@ -30,6 +31,28 @@ from qgis.PyQt import QtWidgets
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'irmis_json_loader_dialog_base.ui'))
+
+now = datetime.now()
+irmishtml_latest = '''
+<a href="http:www.imis.bfs.de/geoportal/q=2022-04-08&amp;type=latest">
+'''
+irmishtml_latest += '''
+  <span style=" text-decoration: underline; color:#0000ff;">http:www.imis.bfs.de/geoportal/?type=latestfoo&measureend=
+'''
+irmishtml_latest += datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
+irmishtml_latest += '''
+</span></a>
+'''
+
+irmishtml_max = '''
+<a href="http:www.imis.bfs.de/geoportal/q=2022-04-08&amp;type=latest">
+'''
+irmishtml_max += '''
+  <span style=" text-decoration: underline; color:#0000ff;">http:www.imis.bfs.de/geoportal/q=2022-04-08&amp;type=maxbar</span>
+'''
+irmishtml_max += '''
+</a>
+'''
 
 
 class IrmisJsonLoaderDialog(QtWidgets.QDialog, FORM_CLASS):
@@ -42,3 +65,6 @@ class IrmisJsonLoaderDialog(QtWidgets.QDialog, FORM_CLASS):
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        self.textBrowser_latest.setHtml(irmishtml_latest)
+        self.textBrowser_max.setHtml(irmishtml_max)
+        print("gui set up")
